ffc778675bd32a811467826aa34dc1bc5c3ae615
Hajdu Tomi <hajdu.tomi@szatyi.hu>
2026-01-16 11:53:28 +0100

    config,style/css valtoztatasok

--- config.php
+++ config.php
@@ -1,8 +1,13 @@
 <?php
-define("DBTYPE","mysql");
-define("DBHOST","localhost");
-define( "DBCHARSET","utf8");
-define("DBNAME","leltar");
-define("DBUSER","root");
-define("DBUSERPW","");
+$host = "localhost";
+$user ="root";
+$password = "";
+$dbname = "leltar";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if($conn-> connect_error)
+{
+    die("Sikertelen adatbázis csatlakozás: ".$conn->connect_error);
+}
 ?>
