04d7cd93e2b087271727a41cb92a6b6b4217363a
Nem <Nem@gmail.com>
2026-01-16 11:50:17 +0100

    added proj files

---
+++ AnnaKisBolt/gyorskodlesok.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnaKisBolt
+{
+    internal class gyorskodlesok
+    {
+        /*
+        Tábla hozzáadás:
+        CREATE TABLE tejtermek ( 
+        id INT PRIMARY KEY AUTO_INCREMENT, 
+        nev VARCHAR(100) NOT NULL, 
+        mennyiseg INT NOT NULL, 
+        mennyiseg_jele VARCHAR(100) NOT NULL, 
+        darabszam INT NOT NULL DEFAULT 0 
+        )
+
+        Termék hozzáadása:
+        INSERT INTO tejtermek (nev, mennyiseg, mennyiseg_jele)
+        VALUES
+        ('Ferrero Kinder Maxi King', '35', 'g'),
+        ('Ferrero Kinder Pingu Tejszelet', '30', 'g');
+         */
+    }
+}
---
+++ src/config.php
@@ -0,0 +1,8 @@
+<?php
+define("DBTYPE","mysql");
+define("DBHOST","localhost");
+define( "DBCHARSET","utf8");
+define("DBNAME","leltar");
+define("DBUSER","root");
+define("DBUSERPW","");
+?>
