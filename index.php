813735dcc2dddbac32dc0a08b54ba6d284da26d5
Malog. <mbalog2006@gmail.com>
2026-01-19 08:07:59 +0100

    no

--- index.php
+++ index.php
@@ -5,16 +5,18 @@
     
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="src/style.css">
+      <link rel="stylesheet" href="src/index.css">
 
       <title>Leltarozo</title>
 
   </head>
 
   <body>
+    
+    <div class="blur"></div>
+    
+      <h1>Main</h1>
 
-    <h1></h1>
-      
   </body>
 
 </html>
