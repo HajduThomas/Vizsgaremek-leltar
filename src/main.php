090cc171ef82c9f59b47d18ae3bd238f331299d3
Malog. <mbalog2006@gmail.com>
2026-01-24 19:05:38 +0100

    created dataTable

--- src/main.php
+++ src/main.php
@@ -30,37 +30,77 @@
           <button>Option 3</button>
         </div>
 
-        <div>
-          <table id="markers">
-            <td>Price</td>
-            <td class="septer">|</td>
-            <td>Stock</td>
-            <td class="septer">|</td>
-            <td>Stuff</td>
-          </table>
-        </div>
-
       </div>
-      <div class="scroller">
-        <div class="content">
-          <h1>mainpanel</h1>
-          <p>This is the main panel</p><br>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus tempor lorem, ac consequat magna eleifend a. Pellentesque at ipsum vestibulum, iaculis mauris eget, egestas ante. Etiam lobortis nulla vestibulum mi dapibus venenatis non in ipsum. Aliquam metus eros, tempor porttitor aliquet id, mattis quis sem. Cras rhoncus condimentum metus eu fringilla. Ut malesuada non neque ut volutpat. Maecenas in dui et erat facilisis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-          <p>
-          Vestibulum convallis sapien id blandit dapibus. Duis auctor est quis consequat fringilla. Sed vehicula ex semper, aliquet libero eu, maximus ligula. Cras libero velit, ullamcorper et sem id, tincidunt ultrices metus. Curabitur et ex felis. Nulla facilisi. Donec mattis, felis quis sollicitudin mollis, lectus massa posuere neque, quis auctor elit eros et arcu. Maecenas nulla ligula, volutpat vel tortor non, fermentum mattis libero. Praesent eget velit eget augue pulvinar posuere id non massa. Mauris placerat ultrices elit tristique finibus. Integer eu volutpat tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed imperdiet neque pharetra neque blandit, et interdum magna mattis. Etiam eros justo, eleifend sed metus in, faucibus vulputate magna. Proin at blandit lorem.
-          </p>
-          <p>
-          Quisque erat libero, scelerisque ut porttitor a, scelerisque sed ex. Sed tempus tempus tellus, sit amet elementum quam sodales sit amet. Curabitur vitae sem id dolor vestibulum efficitur. Praesent luctus ac sapien eu tempor. Vivamus elementum orci sollicitudin, rutrum lacus et, venenatis ipsum. Vivamus malesuada consequat interdum. Sed congue euismod odio. Aenean non ex nulla. Cras a elit at tortor auctor pulvinar. Cras ultricies ante dictum cursus hendrerit.
-          </p>
-          <p>
-          Integer vel elit non neque vulputate accumsan vitae in tortor. Donec ut scelerisque mauris. Aliquam id dolor augue. Aliquam dolor augue, accumsan a vehicula non, laoreet in augue. Nulla volutpat egestas placerat. Maecenas eros nunc, molestie id semper et, mollis sed arcu. Suspendisse at est vel libero bibendum tincidunt. Proin rutrum, enim vitae ornare malesuada, turpis tortor tincidunt quam, vitae placerat nunc magna ac augue. Proin scelerisque lacus eu metus pulvinar, non condimentum ipsum rhoncus. Aliquam venenatis diam vitae sapien gravida consectetur. Integer suscipit viverra ultricies. Donec et lectus fermentum, iaculis massa in, accumsan turpis.
-          </p>
-          <p>
-          Nulla sodales eget eros ac egestas. Aenean lobortis lectus vitae nibh venenatis consequat. Nunc lectus diam, mollis et dui in, auctor dapibus massa. Nunc metus arcu, fringilla in elementum ac, tempor et diam. Mauris at mi eu est pharetra porttitor. Curabitur luctus metus sit amet viverra convallis. Mauris ac risus ornare, pulvinar nunc luctus, suscipit turpis. Sed ornare ut turpis quis eleifend. Phasellus consequat non nibh eu finibus. Nunc non tincidunt ante, vulputate dignissim velit. 
-          </p>
-        </div>
+      <div class="container">
+
+        <table class="content">
+          <tbody id="dataTable">
+          </tbody>
+        </table>
+
+        <script>
+          //filling datatable for testing
+          //if there are a lot of rows, it lags, need to implement "pagination"
+          //basically sectioning the list into parts, like every 200/300 rows
+          //also need an option to choose pagination size
+          const rows = 200;
+          const cols = 9;
+          const dataTable = document.getElementById("dataTable");
+
+          const names = ["eye", "Exodus", "Faint", "Fired Up", "Hold The Night", "Divide My Heart", "How We Roll"];
+          const descs = ["Kanaria", "KoruSe", "Linking Park", "Foret de Vin", "NAOKI", "Hollywoord Undead"];
+          const durs = ["2:14", "3:04", "2:42", "3:26", "3:00", "5:23", "4:45"]
+
+          var row;
+          var col;
+          for (let i = 0; i <= rows; i++) {
+            row = document.createElement("tr");
+            for (let o = 0; o < cols; o++) {
+              col = document.createElement( ((i == 0) ? "th" : "td"));
+              if (i == 0) {
+                switch (o) {
+                  case 0:
+                    col.innerText = "id";
+                    break;
+                  case 1:
+                    col.innerText = "Name";
+                    break;
+                  case 2:
+                    col.innerText = "Description";
+                    break;
+                  case 3:
+                    col.innerText = "Duration";
+                    break;
+                  default:
+                    col.innerText = "Extra";
+                    break;
+                }
+              } else {
+                switch (o) {
+                  case 0:
+                    col.innerText = i;
+                    break;
+                  case 1:
+                    col.innerText = names[i % 7];
+                    break;
+                  case 2:
+                    col.innerText = descs[i % 7];
+                    break;
+                  case 3:
+                    col.innerText = durs[i % 7];
+                    break;
+                  default:
+                    col.innerText = i;
+                    break;
+                }
+              }
+              row.appendChild(col);
+            }
+            //row.style.backgroundColor = (i % 2 == 0) ? "purple" : "magenta";
+            dataTable.appendChild(row);
+          }
+        </script>
+
       </div>
       
     </div>
