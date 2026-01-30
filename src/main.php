8256cb811ac5acb8df958913bc0b9edbf10c54b1
Malog. <mbalog2006@gmail.com>
2026-01-30 23:20:23 +0100

    updated category select

--- src/main.php
+++ src/main.php
@@ -32,10 +32,7 @@
 
         </div>
 
-        <div class="container">
-          <a href="#" class="catButton"><div class="category">category 1</div></a>
-          <a href="#" class="catButton"><div class="category">category 2</div></a>
-          <a href="#" class="catButton"><div class="category">category 3</div></a>
+        <div id="catSelect" class="container">
         </div>
 
       </div>
@@ -74,36 +71,50 @@
         //implement searching function
         //handle first time loading (display the first category as default)
 
-        const rows = 200;
-        const cols = 7;
-
         const dataTable = document.getElementById("dataTable");
+        const catDisplay = document.getElementById("category");
+        const catSelect = document.getElementById("catSelect");
 
         //This is only for testing
         //replace with sql
-        const results = {
-          names: [
-            ["eye", "Exodus", "Faint", "Fired Up", "Hold The Night", "Divide My Heart", "How We Roll"],
-            ["Ghost Rule","Six Black Heavens Guns", "Lowrider", "Echo", "Maw of the King", "Devil Trigger", "Throne", "Kingslayer"],
-            ["Nasty * Nasty * Spell", "God Only Knows", "Judgement", "KILLSWITCH", "Titanium", "WWW", "HUGE W", "Kinetic", "reason"]
-          ],
-          descs: [
-            ["Kanaria", "KoruSe", "Linking Park", "Foret de Vin", "NAOKI", "Hollywoord Undead"],
-            ["Deco*27", "Daisuke Ishiwatari / Naoki Hashimoto", "Cypress Hill", "Crusher-p", "Cami-Cat", "Casey Edwards / Ali Edwards", "Bring Me The Horizon", "Bring Me The Horizon / BABYMETAL"],
-            ["Camellia", "bitbreaker / Kasane Teto", "meganeko", "mekaloton", "Mittsies", "Moe Shop / Edoga-Sullivan", "Mori Calliope", "Pete Cottrell", "Rad Cat"]
-          ],
-          durs: [
-            ["2:14", "3:04", "2:42", "3:26", "3:00", "5:23", "4:45"],
-            ["3:30", "4:43", "4:36", "3:50", "3:41", "6:45", "3:11", "3:40"],
-            ["4:24", "4:47", "5:29", "2:15", "3:06", "3:30", "4:09", "3:49", "2:45"]
-          ]
-        }
+        const rows = 200;
+        const cols = 7;
+
+        const results = [
+          {
+            catName: "music", //table name
+            //table contents
+            names: ["eye", "Exodus", "Faint", "Fired Up", "Hold The Night", "Divide My Heart", "How We Roll"],
+            descs: ["Kanaria", "KoruSe", "Linking Park", "Foret de Vin", "NAOKI", "Hollywoord Undead"],
+            durs: ["2:14", "3:04", "2:42", "3:26", "3:00", "5:23", "4:45"]
+          },
+          {
+            catName: "more music",
+            names: ["Ghost Rule","Six Black Heavens Guns", "Lowrider", "Echo", "Maw of the King", "Devil Trigger", "Throne", "Kingslayer"],
+            descs: ["Deco*27", "Daisuke Ishiwatari / Naoki Hashimoto", "Cypress Hill", "Crusher-p", "Cami-Cat", "Casey Edwards / Ali Edwards", "Bring Me The Horizon", "Bring Me The Horizon / BABYMETAL"],
+            durs: ["3:30", "4:43", "4:36", "3:50", "3:41", "6:45", "3:11", "3:40"]
+          },
+          {
+            catName: "still music",
+            names: ["Nasty * Nasty * Spell", "God Only Knows", "Judgement", "KILLSWITCH", "Titanium", "WWW", "HUGE W", "Kinetic", "reason"],
+            descs: ["Camellia", "bitbreaker", "meganeko", "mekaloton", "Mittsies", "Moe Shop / Edoga-Sullivan", "Mori Calliope", "Pete Cottrell", "Rad Cat"],
+            durs: ["4:24", "4:47", "5:29", "2:15", "3:06", "3:30", "4:09", "3:49", "2:45"]
+          },
+          {
+            catName: "yup, music",
+            names: ["Latin Lingo", "Purple Pills", "A Blast Beat", "Shield Sister", "Lagtrain", "Levitate", "MoeChakkaFire", "Methods of Madness", "Extras (city pop cover)"],
+            descs: ["Cypress Hill", "D12", "Dune", "Garrett Williamson", "girl_dm_ / darkbluecat", "Hollywood Undead", "issey", "James Landino", "jen"],
+            durs: ["3:58", "5:04", "3:05", "2:35", "4:11", "3:24", "2:35", "1:58", "6:07"]
+          }
+        ]
 
         function fillTable(id) {
           
           var row;
           var col;
           var tbody = document.createElement("tbody")
+
+          catDisplay.innerText = results[id].catName;
 
           for (let i = 0; i <= rows; i++) {
             row = document.createElement("tr");
@@ -133,14 +144,13 @@
                     col.innerText = i;
                     break;
                   case 1:
-                    console.log(id);
-                    col.innerText = results.names[id][(i-1) % results.names[id].length];
+                    col.innerText = results[id].names[(i-1) % results[id].names.length];
                     break;
                   case 2:
-                    col.innerText = results.descs[id][(i-1) % results.descs[id].length];
+                    col.innerText = results[id].descs[(i-1) % results[id].descs.length];
                     break;
                   case 3:
-                    col.innerText = results.durs[id][(i-1) % results.durs[id].length];
+                    col.innerText = results[id].durs[(i-1) % results[id].durs.length];
                     break;
                   default:
                     col.innerText = i;
@@ -148,6 +158,7 @@
                 }
               }
               row.appendChild(col);
+              row.id = i;
             }
             tbody.appendChild(row);
           }
@@ -158,28 +169,35 @@
 
         fillTable(0);
 
-
-        const catDisplay = document.getElementById("category");
-
+        //I don't know how else to do this ;-;
         function changeCat(event) {
           var target = event.currentTarget;
-          catDisplay.innerText = target.name;
-          fillTable(target.id);
+          fillTable(target.cat);
         }
-
-        const categories = document.getElementsByClassName("catButton");
-
-        const catNames = ["music","more music","still music"];
         
-        for (let i = 0; i < categories.length; i++) {
-          const element = categories[i];
-          element.addEventListener("click", changeCat);
-          element.name = catNames[i];
-          element.firstChild.innerText = element.name;
-          element.id = i;
+        //
+        for (let i = 0; i < results.length; i++) {
+          /*
+          //alt, might use
+          var catButton = document.createElement("button");
+          catButton.className = "category";
+          catButton.addEventListener("click", changeCat);
+          catButton.innerText = results[i].catName;
+          catButton.id = i;
+          */
+          var catButton = document.createElement("a");
+          var catDiv = document.createElement("div");
+          catDiv.className = "category";
+          catDiv.innerText = results[i].catName;
+          catButton.appendChild(catDiv);
+          catButton.href = "#0";
+          catButton.addEventListener("click", changeCat);
+          catButton.cat = i;
+
+          catSelect.appendChild(catButton);
         }
 
-        catDisplay.innerText = catNames[0];
+        catDisplay.innerText = results[0].catName;
         
       </script>
 
